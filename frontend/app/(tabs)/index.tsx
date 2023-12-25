179265ff5bc8045ef920e5f84e6e8d732781148c
joseph <josephomotade0@gmail.com>
2023-12-25 13:52:00 -0800

    commit

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome5 } from "@expo/vector-icons";
+import AllSongs from "../../components/expolore/AllSongs";
+import AllAlbums from "../../components/expolore/AllAlbums";
+import AllArtist from "../../components/expolore/AllArtist";
+import { router } from "expo-router";
+import { useAuth } from "../../context/AuthContext";
+import { DocumentData, doc, getDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase";
+import { User as FirebaseAuthUser } from "firebase/auth";
+import UserAvatar from "react-native-user-avatar";
+import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
+
+export default function TabOneScreen() {
+  const [data, setData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { userData } = useAuth();
+
+  const fetchUserData = async (user: FirebaseAuthUser) => {
+    setIsLoading(true);
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      setData(docSnap);
+      router.push("/(tabs)");
+      setIsLoading(false);
+      return docSnap.data();
+    } else {
+      router.push("/(auth)/");
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      fetchUserData(user);
+    }
+  }, []);
+
+  return (
+    <SafeAreaView className="min-h-screen flex-1">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{
+          marginBottom: 100,
+          flex: 1,
+          minHeight: "100%",
+        }}
+      >
+        {/** header view */}
+        <View className="pb-[40px]">
+          <View className="flex-row items-center p-3 justify-between">
+            <View className="flex-row items-start space-x-4">
+              <Pressable onPress={() => router.push("/profile")}>
+                <UserAvatar
+                  size={50}
+                  name="Avishay Bar"
+                  textStyle={{
+                    fontSize: 10,
+                  }}
+                />
+              </Pressable>
+              <View className="space-y-1">
+                <ShimmerPlaceHolder
+                  visible={!isLoading}
+                  style={{width: 200, height: 20}}
+                >
+                  <Text className="text-[18px] font-semibold text-[#fff]">
+                    {data?.data()?.email.split("@")[0]}
+                  </Text>
+                </ShimmerPlaceHolder>
+                <ShimmerPlaceHolder
+
+                  visible={!isLoading}
+                  style={{width: 200, height: 20}}
+                >
+                  <Text className="text-[14px] text-[#DEDEDE] font-medium">
+                    {data?.data()?.email}
+                  </Text>
+                </ShimmerPlaceHolder>
+              </View>
+            </View>
+            <View className="flex-row items-center space-x-5">
+              <FontAwesome5 name="bell" color="#fff" size={20} />
+              <Pressable onPress={() => router.push("/create")} className="">
+                <FontAwesome5 name="plus" color="#fff" size={20} />
+              </Pressable>
+            </View>
+          </View>
+          <View className="flex-row items-center p-3 justify-between">
+            <Text className="text-[26px] font-semibold text-[#fff] w-[50%]">
+              Listen The Latest Musics
+            </Text>
+            <View className="bg-transparent border border-[#fff] flex-row items-center space-x-3 w-[50%] p-3 rounded-[40px]">
+              <FontAwesome5 name="search" color="#fff" size={10} />
+              <TextInput
+                placeholder="Search"
+                placeholderTextColor="#fff"
+                className="w-fit text-[#fff]"
+              />
+            </View>
+          </View>
+        </View>
+        {/* <CarouselCompoent /> */}
+        <View className="space-y-[20px] flex-1 min-h-screen">
+          <AllSongs />
+          <AllAlbums />
+          <AllArtist />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
---
+++ frontend/context/AuthContext.tsx
@@ -0,0 +1,283 @@
+import { router, useNavigation, useSegments } from "expo-router";
+import React, { useEffect, useState } from "react";
+import {
+  createAccount,
+  getAccount,
+  getAccountPhrase,
+  permanentlyDeleteAccount,
+} from "@rly-network/mobile-sdk";
+import { Alert } from "react-native";
+import {
+  User as FirebaseAuthUser,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+// import { auth } from "../firebase";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { ethers } from "ethers";
+import { Audio } from "expo-av";
+import { auth, db } from "../firebase";
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  doc,
+  getDoc,
+  setDoc,
+} from "firebase/firestore";
+
+type Session = string | undefined;
+
+interface DataItem {
+  id: string;
+  image: string;
+  name: string;
+  current_price: number;
+  symbol: string;
+}
+
+type AuthContextValue = {
+  session: Session;
+  createAnEOA: (email: string, password: string) => Promise<boolean>;
+  permanentlyDeleteAccount: () => Promise<void>;
+  signin: (
+    email: string,
+    password: string
+  ) => Promise<DocumentData | undefined>;
+  playSound(external_url: string, item: any): Promise<void>;
+  playerOpen: boolean;
+  currentlyPlayed: {
+    image: string;
+    title: string;
+    url: string;
+    artist: string;
+  };
+  stopSound(external_url: string): Promise<void>;
+  pauseSound(): Promise<void>;
+  isPlaying: boolean;
+  userData: QueryDocumentSnapshot<DocumentData, DocumentData> | undefined;
+  action: string;
+};
+
+const AuthContext = React.createContext<AuthContextValue>({
+  session: undefined,
+  createAnEOA: async (email: string, password: string) => {
+    // Default implementation, you may want to handle this differently
+    console.warn("createAnEOA function not implemented");
+    return false; // return a boolean value
+  },
+  permanentlyDeleteAccount: async () => {
+    // Default implementation, you may want to handle this differently
+    console.warn("permanentlyDeleteAccount function not implemented");
+  },
+  signin: async (
+    email: string,
+    password: string
+  ): Promise<DocumentData | undefined> => {
+    // Default implementation, you may want to handle this differently
+    console.warn("Signin function not implemented");
+    return undefined; // return undefined as per the expected return type
+  },
+  playSound: async (external_url: string, item: any): Promise<void> => {
+    // Implementation goes here
+  },
+  playerOpen: false,
+  currentlyPlayed: {
+    image: "",
+    title: "",
+    url: "",
+    artist: "",
+  },
+  stopSound: async (external_url: string): Promise<void> => {
+    // Implementation goes here
+  },
+  pauseSound(): Promise<void> {
+    return Promise.resolve();
+  },
+  isPlaying: false,
+  userData: undefined,
+  action: "",
+});
+
+export function useAuth() {
+  return React.useContext(AuthContext);
+}
+
+function useProtectedRoute(session: Session) {
+  const segments = useSegments();
+
+  useEffect(() => {
+    // const inAuthGroup = segments[0] === "(auth)";
+    if (!session) {
+      router.replace("/");
+    } else if (session) {
+      router.replace("/(tabs)");
+    }
+  }, [session]);
+}
+
+type AuthProviderProps = {
+  // createUserWithEmailAndPassword: (auth: any, email: string, password: string) => Promise<any>;
+  children: React.ReactNode;
+};
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [session, setSession] = useState<Session>();
+  const [playerOpen, setPlayerOpened] = useState(false);
+  const [currentlyPlayed, setCurrentLyPlayed] = useState({
+    image: "",
+    title: "",
+    url: "",
+    artist: "",
+  });
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [userData, setUser] =
+    useState<QueryDocumentSnapshot<DocumentData, DocumentData>>();
+  console.log("user", userData);
+  const [action, setAction] = useState("");
+  useProtectedRoute(userData?.id);
+
+  const createAnEOA = async (email: string, password: string) => {
+    let userCredential, newAccount;
+
+    try {
+      setAction("Signing User....");
+      // Create a new user with email and password
+      userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+    } catch (error) {
+      console.error("Error creating user: ", error);
+      return false;
+    }
+
+    const user = userCredential.user;
+
+    if (user) {
+      try {
+        setAction("Creating Wallet....");
+        newAccount = await createAccount({
+          storageOptions: {
+            saveToCloud: false,
+            rejectOnCloudSaveFailure: true,
+          },
+          overwrite: true,
+        });
+      } catch (error) {
+        console.error("Error creating account: ", error);
+        return false;
+      }
+
+      if (newAccount) {
+        try {
+          setAction("Storing your info.....");
+          // Store the user's email and wallet address in Firestore
+          const docSnap = await setDoc(doc(db, "users", user.uid), {
+            email: email,
+            walletAddress: newAccount,
+          });
+          return true;
+        } catch (error) {
+          console.error("Error storing user data in Firestore: ", error);
+          return false;
+        }
+      }
+    }
+
+    console.log("User registered successfully");
+    setAction("Account created Sucessfully 😁");
+    return true;
+  };
+
+  // Add a state for the current sound object
+  const [soundObject, setSoundObject] = useState<Audio.Sound | null>(null);
+
+  async function playSound(external_url: string, item: any) {
+    // Stop the currently playing sound if there is one
+    if (soundObject) {
+      await soundObject.stopAsync();
+    }
+
+    setPlayerOpened(true);
+    setCurrentLyPlayed({
+      image: item.image,
+      title: item.name,
+      url: item.external_url,
+      artist: item.artist,
+    });
+    await Audio.setAudioModeAsync({
+      playsInSilentModeIOS: true,
+      staysActiveInBackground: true, // Add this line
+    });
+    try {
+      const { sound: playbackObject } = await Audio.Sound.createAsync(
+        {
+          uri: external_url,
+        },
+        { shouldPlay: true }
+      );
+
+      setSoundObject(playbackObject); // Save the sound object
+      await playbackObject.playAsync();
+      setIsPlaying(true);
+    } catch (error) {
+      console.log("Error loading audio", error);
+    }
+  }
+
+  async function pauseSound() {
+    if (soundObject) {
+      await soundObject.pauseAsync();
+      setIsPlaying(false);
+    }
+  }
+
+  async function stopSound() {
+    if (soundObject) {
+      await soundObject.stopAsync();
+      setIsPlaying(false);
+      setSoundObject(null); // Clear the sound object
+    }
+  }
+
+
+
+  const signin = async (email: string, password: string) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      return user;
+    } catch (error) {
+      console.error("Error signing in: ", error);
+    }
+  };
+
+  const contextValue: AuthContextValue = {
+    session,
+    createAnEOA,
+    permanentlyDeleteAccount,
+    playSound,
+    currentlyPlayed,
+    playerOpen,
+    stopSound,
+    isPlaying,
+    pauseSound,
+    signin,
+    userData,
+    action,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+}
+
+// 0x9ab0e4dd0ad0abc732c0d8eb6fe70ae5aa77a79fcb586789d2aaec94d91c3c46;
+// e26226
